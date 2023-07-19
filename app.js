4129d1f974b03bc088189bb0408de4d0a215c345
yusufhalby <yosufkhaled@eng.aswu.edu.eg>
2023-07-19 17:49:20 +0300

    starting with graphQL

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const {graphqlHTTP} = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/messages?retryWrites=true&w=majority';
 const port = process.env.PORT || 8080;
@@ -27,15 +27,18 @@ const fileFilter = (req, file, cb) => {
         case 'image/png':
         case 'image/jpg':
         case 'image/jpeg':
-            cb(null, true);     //accept the file
+            cb(null, true); //accept the file
             break;
         default:
-            cb(null, false);    //reject the file
+            cb(null, false); //reject the file
     }
-};  
+};
 
 app.use(bodyParser.json());
-app.use(multer({storage: fileStorage, fileFilter}).single('image'));
+app.use(multer({
+    storage: fileStorage,
+    fileFilter
+}).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // CORS Headers - Cross-Origin Resource Sharing 
@@ -43,18 +46,43 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE'); //OPTIONS is optional :"
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); //Authorization must be enabled
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true,
+        customFormatErrorFn(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error occurred.';
+            const code = err.originalError.code || 500;
+            return {
+                message: message,
+                status: code,
+                data: data
+            };
+        }
+    })
+);
 
-app.use((error, req, res, next)=>{
+app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500; //setting default value of 500
     const message = error.message;
     const data = error.data;
-    res.status(status).json({message: message, data});
+    res.status(status).json({
+        message: message,
+        data
+    });
 
 });
 
@@ -62,11 +90,7 @@ app.use((error, req, res, next)=>{
 mongoose
     .connect(MONGODB_URI)
     .then(result => {
-        const server = app.listen(port);
-        const io = require('./socket').init(server);
-        io.on('connection', socket => {
-            console.log('Client connected');
-        });
+        app.listen(port);
         console.log('Server is connected on port: ' + port);
     })
     .catch(err => {
